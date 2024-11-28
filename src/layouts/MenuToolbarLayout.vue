d467e4eef51caeea9555351b6e4961aa59e98592
Fabo2201 <andersonfabian2201@gmail.com>
2024-11-28 01:47:53 -0300

    Agregue las rutas y subi los detalles del caht

---
+++ src/layouts/MenuToolbarLayout.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="q-pa-md">
+
+    <q-header
+      elevated
+      :style="{padding: '0 20px', backgroundColor: '#085d71'}"
+      class="row justify-between items-center"
+    >
+      <div>
+        <q-toolbar class="row items-center">
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        </q-toolbar>
+      </div>
+
+      <div
+        class="q-mx-auto"
+        style="display: flex; justify-content: center"
+      >
+        <img
+          src="src/assets/LogoCamcel.jpg"
+          alt="Logo Camcel"
+          width="287"
+          height="65"
+        />
+      </div>
+
+     
+      <div class="row">
+        <Notificaciones2/>
+        <chat/>
+        <q-btn flat icon="person">
+          <q-menu>
+            <q-list>
+              <q-item clickable v-close-popup to="/Configuracion">
+                <q-item-section>Configuracion</q-item-section>
+              </q-item>
+              <q-item clickable @click="handleLogout" v-close-popup>
+                <q-item-section >Cerrar Sesion</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+      </div>
+    </q-header>
+    
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :width="200"
+      :breakpoint="500"
+      bordered
+      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+    >
+      <q-scroll-area class="fit">
+        <q-list>
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item
+              clickable
+              v-ripple
+              class="text-black"
+              :to="{ name: menuItem.href }"
+            >
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+    <q-page-container>
+      <q-page class="q-mx-auto" style="max-width: 2000px;">
+        <slot/>
+      </q-page>
+    </q-page-container>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Chat from 'src/components/Chat.vue'
+import notificaciones from 'src/components/Notificaciones.vue' 
+import Notificaciones2 from 'src/components/Notificaciones2.vue';
+
+export default {
+  components: {
+    Chat,
+    notificaciones,
+    Notificaciones2
+  },
+  setup() {
+    const search = ref("");
+
+    const menuList = [
+      {
+        icon: "business_center",
+        label: "Empresas",
+        href: "enterprises",
+        separator: false,
+      },
+      {
+        icon: "mdi-account",
+        label: "Usuarios",
+        href: "users",
+
+      },
+      { 
+        icon: "mdi-folder-multiple",
+        label: "His.trabajo",
+        href: "jobs",
+        separator: false,
+      },
+      { 
+       label: "Trabajos", 
+       icon: "mdi-account-hard-hat", 
+       href: "jobs",
+       separator: false,
+      },
+      {
+        label: "Soporte",
+        icon: "mdi-cog-outline",
+        href: "soporte",
+        separator: false,
+      },
+    ];
+
+    return {
+      drawer: ref(false),
+      search,
+      menuList, 
+    };
+  }
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,8 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("src/pages/Empresas.vue") }],
   },
   {
     path: '/login',
@@ -19,6 +17,48 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Registro.vue') }
     ]
+  },
+  {
+    path: '/his.trabajo/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/HisTrabajo.vue'),
+        name: "His.trabajos",
+       },
+    ],
+  },
+  {
+    path: "/usuarios/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("src/pages/Users.vue"),
+        name: "users",
+      },
+    ],
+  },
+  {
+    path: "/trabajos/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("src/pages/Trabajo.vue"),
+        name: "jobs",
+      },
+    ],
+  },
+  {
+    path: "/Configuracion",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("src/pages/UsuarioConfig.vue"),
+        name: "Configuracion",
+      },
+    ],
   },
   {
     path: "/empresas/",
@@ -38,57 +78,6 @@ const routes = [
         path: ":enterprise/operator/:pk/",
         component: () => import("src/pages/OperatorDetail.vue"),
         name: "operators-detail",
-      },
-    ],
-  },
-  {
-    path: "/usuarios/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("src/pages/Users.vue"),
-        name: "users",
-      },
-    ],
-  },
-  {
-    path: "/trabajos",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("src/pages/Trabajo.vue"),
-        name: "trabajos",
-      },
-    ],
-  },
-  {
-    path: "/his.trabajo/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("src/pages/HisTrabajo.vue"),
-        name: "his.trabajo",
-      },
-    ],
-  },
-  {
-    path: "/cuenta/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      
-    ],
-  },
-  {
-    path: "/Configuracion",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("src/pages/UsuarioConfig.vue"),
-        name: "Configuracion",
       },
     ],
   },
